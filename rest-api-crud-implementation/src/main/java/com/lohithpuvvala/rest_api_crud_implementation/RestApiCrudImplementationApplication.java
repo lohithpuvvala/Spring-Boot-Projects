3f8baa9c19d02a39674d65ff25834f72781121c3
lohithpuvvala <lohithpuvvala@gmail.com>
2026-01-12 15:13:56 +0530

    Create Project 1: rest api implementation. Add Spring Boot App from Spring Initializer

---
+++ rest-api-crud-implementation/src/main/java/com/lohithpuvvala/rest_api_crud_implementation/RestApiCrudImplementationApplication.java
@@ -0,0 +1,11 @@
+package com.lohithpuvvala.rest_api_crud_implementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiCrudImplementationApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiCrudImplementationApplication.class, args);
+	}
+}
---
+++ rest-api-crud-implementation/src/test/java/com/lohithpuvvala/rest_api_crud_implementation/RestApiCrudImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lohithpuvvala.rest_api_crud_implementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiCrudImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
