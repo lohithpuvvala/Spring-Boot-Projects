1582b375d15d92a336c4c993e83bfb8c95385ac4
lohithpuvvala <lohithpuvvala@gmail.com>
2026-01-22 20:43:49 +0530

    Create Model Folder. Add CloudVendor Model with constructors, getters and setters

--- rest-api-crud-implementation/src/main/java/com/lohithpuvvala/rest_api_crud_implementation/RestApiCrudImplementationApplication.java
+++ rest-api-crud-implementation/src/main/java/com/lohithpuvvala/rest_api_crud_implementation/RestApiCrudImplementationApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestApiCrudImplementationApplication {
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestApiCrudImplementationApplication.class, args);
 	}
 }
