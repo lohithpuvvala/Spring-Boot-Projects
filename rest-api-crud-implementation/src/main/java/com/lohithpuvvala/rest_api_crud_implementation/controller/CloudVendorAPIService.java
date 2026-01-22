010a16595bb3a1583eeb7019af24f0f1277ca59f
lohithpuvvala <lohithpuvvala@gmail.com>
2026-01-22 21:08:41 +0530

    Create Controller Folder. Add CloudVendorAPIService Controller for CRUD operations

---
+++ rest-api-crud-implementation/src/main/java/com/lohithpuvvala/rest_api_crud_implementation/controller/CloudVendorAPIService.java
@@ -0,0 +1,39 @@
+package com.lohithpuvvala.rest_api_crud_implementation.controller;
+
+import com.lohithpuvvala.rest_api_crud_implementation.model.CloudVendor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cloudvendor")
+public class CloudVendorAPIService
+{
+
+    CloudVendor cloudVendor;
+
+    //Used to retrive Cloud Vendor Details by vendorID
+    @GetMapping("{vendorId}")
+    public CloudVendor getCloudVendorDetails(String vendorId){
+        return  cloudVendor;
+    }
+
+    //Creates New Vendor Details
+    @PostMapping
+    public String createCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Created Successfully";
+    }
+
+    //Updates Vendor Details
+    @PutMapping
+    public String updateCloudVendorDetails(@RequestBody CloudVendor cloudVendor){
+        this.cloudVendor = cloudVendor;
+        return "Cloud Vendor Updated Successfully";
+    }
+
+    //Deletes Vendor Details
+    @DeleteMapping("{vendorId}")
+    public String deleteCloudVendorDetails(String vendorId){
+        this.cloudVendor = null;
+        return "Cloud Vendor Deleted Successfully";
+    }
+}
