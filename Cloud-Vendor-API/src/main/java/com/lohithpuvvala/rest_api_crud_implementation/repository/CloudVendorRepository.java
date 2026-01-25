54909be706fe9a5817601776ded2d35fd248557a
lohithpuvvala <lohithpuvvala@gmail.com>
2026-01-25 18:49:25 +0530

    Add Repository Layer. Create CloudVendorRepository Interface

---
+++ Cloud-Vendor-API/src/main/java/com/lohithpuvvala/rest_api_crud_implementation/repository/CloudVendorRepository.java
@@ -0,0 +1,6 @@
+package com.lohithpuvvala.rest_api_crud_implementation.repository;
+
+import com.lohithpuvvala.rest_api_crud_implementation.model.CloudVendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CloudVendorRepository extends JpaRepository<CloudVendor, String> { }
