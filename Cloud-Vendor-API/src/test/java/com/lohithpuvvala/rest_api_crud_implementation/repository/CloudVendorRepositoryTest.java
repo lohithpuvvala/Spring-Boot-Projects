5b5c0bb8a1aae4923f0e817ecdbce91b79cb8d32
lohithpuvvala <lohithpuvvala@gmail.com>
2026-02-23 23:36:10 +0530

    Create CloudVendorRepositoryTest class. Add setUp and tearDown functions

--- Cloud-Vendor-API/src/test/java/com/lohithpuvvala/rest_api_crud_implementation/repository/CloudVendorRepositoryTest.java
+++ Cloud-Vendor-API/src/test/java/com/lohithpuvvala/rest_api_crud_implementation/repository/CloudVendorRepositoryTest.java
@@ -1,8 +1,33 @@
 package com.lohithpuvvala.rest_api_crud_implementation.repository;
 
+import com.lohithpuvvala.rest_api_crud_implementation.model.CloudVendor;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
 
 @DataJpaTest
 public class CloudVendorRepositoryTest {
 
+    @Autowired
+    private CloudVendorRepository cloudVendorRepository;
+    CloudVendor cloudVendor;
+
+    @BeforeEach
+    void setUp() {
+        cloudVendor = new CloudVendor(
+                "1","Amazon", "USA","xxxxx"
+        );
+
+        cloudVendorRepository.save(cloudVendor);
+    }
+
+    @AfterEach
+    void tearDown() {
+        cloudVendor = null;
+        cloudVendorRepository.deleteAll();
+    }
+
+    //Test case Success
+    //Test case Failure
 }
