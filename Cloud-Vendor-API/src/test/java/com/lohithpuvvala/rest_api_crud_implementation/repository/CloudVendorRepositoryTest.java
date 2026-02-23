cdc0f6073afa74ec15d11eaf675767c2a9fe09e6
lohithpuvvala <lohithpuvvala@gmail.com>
2026-02-23 23:56:57 +0530

    Add Test testFindByVendorName_Found and testFindByVendorName_NotFound

--- Cloud-Vendor-API/src/test/java/com/lohithpuvvala/rest_api_crud_implementation/repository/CloudVendorRepositoryTest.java
+++ Cloud-Vendor-API/src/test/java/com/lohithpuvvala/rest_api_crud_implementation/repository/CloudVendorRepositoryTest.java
@@ -3,8 +3,13 @@ package com.lohithpuvvala.rest_api_crud_implementation.repository;
 import com.lohithpuvvala.rest_api_crud_implementation.model.CloudVendor;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @DataJpaTest
 public class CloudVendorRepositoryTest {
@@ -29,5 +34,21 @@ public class CloudVendorRepositoryTest {
     }
 
     //Test case Success
+
+    @Test
+    void testFindByVendorName_Found(){
+        List<CloudVendor>cloudVendorList = cloudVendorRepository.findByVendorName("Amazon");
+        assertThat(cloudVendorList.get(0).getVendorId())
+                .isEqualTo(cloudVendor.getVendorId());
+        assertThat(cloudVendorList.get(0).getVendorAddress())
+                .isEqualTo(cloudVendor.getVendorAddress());
+
+    }
+
     //Test case Failure
+    @Test
+    void testFindByVendorName_NotFound(){
+        List<CloudVendor>cloudVendorList = cloudVendorRepository.findByVendorName("GCP");
+        assertThat(cloudVendorList.isEmpty()).isTrue();
+    }
 }
