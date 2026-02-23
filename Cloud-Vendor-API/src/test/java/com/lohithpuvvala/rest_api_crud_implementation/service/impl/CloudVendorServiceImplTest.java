bc02bd80914efc6ecc5e4a03868e701f5fac2388
lohithpuvvala <lohithpuvvala@gmail.com>
2026-02-24 00:00:32 +0530

    Create CloudVendorServiceImplTest class. Generate required function skeleton

---
+++ Cloud-Vendor-API/src/test/java/com/lohithpuvvala/rest_api_crud_implementation/service/impl/CloudVendorServiceImplTest.java
@@ -0,0 +1,42 @@
+package com.lohithpuvvala.rest_api_crud_implementation.service.impl;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CloudVendorServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void createCloudVendor() {
+    }
+
+    @Test
+    void updateCloudVendor() {
+    }
+
+    @Test
+    void deleteCloudVendor() {
+    }
+
+    @Test
+    void getCloudVendorById() {
+    }
+
+    @Test
+    void findByVendorName() {
+    }
+
+    @Test
+    void getAllCloudVendors() {
+    }
+}
