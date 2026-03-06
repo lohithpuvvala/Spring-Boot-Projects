0f7f6fcb6fc0932308c6a5b561cf1dedffd6b0bd
lohithpuvvala <lohithpuvvala@gmail.com>
2026-03-07 00:21:40 +0530

    Project 2: Online Book Store with MongoDB

---
+++ Online-Book-Store/src/main/java/com/lohithpuvvala/Online_Book_Store/OnlineBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.lohithpuvvala.Online_Book_Store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBookStoreApplication.class, args);
+	}
+
+}
---
+++ Online-Book-Store/src/test/java/com/lohithpuvvala/Online_Book_Store/OnlineBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lohithpuvvala.Online_Book_Store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
