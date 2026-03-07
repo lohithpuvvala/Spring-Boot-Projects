48ccb627dc7dbc1f510d5708f80802c04a42e219
lohithpuvvala <lohithpuvvala@gmail.com>
2026-03-07 14:45:40 +0530

    Create BookService Interface in Service Layer

---
+++ Online-Book-Store/src/main/java/com/lohithpuvvala/Online_Books_Store/service/BookService.java
@@ -0,0 +1,14 @@
+package com.lohithpuvvala.Online_Books_Store.service;
+
+import com.lohithpuvvala.Online_Books_Store.dto.BookDto;
+
+import java.util.List;
+
+public interface BookService {
+
+    public BookDto getBook(String bookId);
+    public List<BookDto> getAllBooks();
+    public BookDto createBook(BookDto bookDto);
+    public BookDto updateBook(BookDto bookDto);
+    public void deleteBookByBookId(String bookId);
+}
