fdb85fa180b2a1b531235089abbd037ace8c2c2d
lohithpuvvala <lohithpuvvala@gmail.com>
2026-03-07 23:34:34 +0530

    Add UserInfo Class in Entity Layer

---
+++ Online-Book-Store/src/main/java/com/lohithpuvvala/Online_Books_Store/entity/UserInfo.java
@@ -0,0 +1,14 @@
+package com.lohithpuvvala.Online_Books_Store.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserInfo {
+    private String username;
+    private String password;
+    private String  roles;
+}
