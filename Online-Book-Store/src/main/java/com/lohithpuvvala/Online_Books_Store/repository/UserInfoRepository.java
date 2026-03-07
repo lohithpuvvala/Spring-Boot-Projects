54a701c82bc939528a1fd1a1e991409129514a59
lohithpuvvala <lohithpuvvala@gmail.com>
2026-03-07 23:48:53 +0530

    Add UserInfoRepository Interface in Repository Layer

---
+++ Online-Book-Store/src/main/java/com/lohithpuvvala/Online_Books_Store/repository/UserInfoRepository.java
@@ -0,0 +1,11 @@
+package com.lohithpuvvala.Online_Books_Store.repository;
+
+import com.lohithpuvvala.Online_Books_Store.entity.UserInfo;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends MongoRepository<UserInfo, String> {
+
+    Optional<UserInfo> findByUserName(String userName);
+}
