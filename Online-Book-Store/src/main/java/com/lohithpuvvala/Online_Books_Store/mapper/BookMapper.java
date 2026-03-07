5b7ea59ff60bb978a948fc4237bbbbf202acbf76
lohithpuvvala <lohithpuvvala@gmail.com>
2026-03-07 13:43:11 +0530

    Create BookMapper class in Mapper layer

---
+++ Online-Book-Store/src/main/java/com/lohithpuvvala/Online_Books_Store/mapper/BookMapper.java
@@ -0,0 +1,29 @@
+package com.lohithpuvvala.Online_Books_Store.mapper;
+
+import com.lohithpuvvala.Online_Books_Store.dto.BookDto;
+import com.lohithpuvvala.Online_Books_Store.entity.Book;
+
+public class BookMapper {
+
+    public static BookDto toDto(Book book){
+        BookDto bookDto = new BookDto(
+                                book.bookId(),
+                                book.name(),
+                                book.price(),
+                                book.author(),
+                                book.description()
+                            );
+        return bookDto;
+    }
+
+    public static Book toBook(BookDto bookDto){
+    Book book = new Book(
+                    bookDto.bookId(),
+                    bookDto.name(),
+                    bookDto.price(),
+                    bookDto.author(),
+                    bookDto.description()
+                );
+    return book;
+    }
+}
