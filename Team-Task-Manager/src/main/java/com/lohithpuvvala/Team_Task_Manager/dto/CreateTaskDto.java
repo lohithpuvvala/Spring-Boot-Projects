9e0a4ca89d625c2908ee05ade79327a59f7f510b
lohithpuvvala <lohithpuvvala@gmail.com>
2026-03-14 00:27:22 +0530

    Create CreateTaskDTO in dto layer

---
+++ Team-Task-Manager/src/main/java/com/lohithpuvvala/Team_Task_Manager/dto/CreateTaskDto.java
@@ -0,0 +1,28 @@
+package com.lohithpuvvala.Team_Task_Manager.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class CreateTaskDto {
+    private String title;
+    private String description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
