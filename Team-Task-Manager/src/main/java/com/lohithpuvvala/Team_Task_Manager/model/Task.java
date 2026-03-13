ed606bd654d18715fbdf4e8a1d9904fffd84a07e
lohithpuvvala <lohithpuvvala@gmail.com>
2026-03-14 00:10:31 +0530

    Create Task Model class. Add Lombok Dependency

---
+++ Team-Task-Manager/src/main/java/com/lohithpuvvala/Team_Task_Manager/model/Task.java
@@ -0,0 +1,21 @@
+package com.lohithpuvvala.Team_Task_Manager.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String title;
+    String description;
+}
